d518b631de602f75f06f267ffe9b27bc5709d4e8
jeremyacoop <63517273+jeremyacoop@users.noreply.github.com>
2022-02-27 21:48:59 -0600

    Set up files, filled out basic data & links

---
+++ src/components/App.jsx
@@ -0,0 +1,66 @@
+// "dependencies": {
+//     "react": "^17.0.1",
+//     "react-dom": "^17.0.1",
+//     "react-redux": "^7.2.6",
+//     "react-scripts": "^5.0.0",
+//     "redux": "^4.1.2",
+//     "redux-logger": "^3.0.6"
+
+import { useState, useEffect } from "react";
+import './App.css';
+
+function App() {
+
+    // Work history
+    // Education
+
+    return (
+        <div>
+            <header>
+                <h1>Jeremy Cooper</h1>
+
+            </header>
+            <body>
+                <h2>
+                    Contact:
+                </h2>
+                <div>
+                    <a target='_blank' href='https://www.linkedin.com/in/jeremy-cooper-848911230/' >Linkedin</a>
+                </div>
+                <div>
+                    <a target='_blank' href='https://github.com/jeremyacoop/jeremyacoop.github.io' >Github</a>
+                </div>
+                <h2>
+                    Portfolio
+                </h2>    
+                <div>
+                    <h3>
+                        <a target='_blank' href='https://github.com/jeremyacoop/prime-solo-project' > 
+                        Madness to Method
+                        </a>
+                    </h3>
+
+                </div>
+                <div>
+                    <h3>
+                        <a target='_blank' href='https://github.com/jeremyacoop/weekend-sql-todo-list'>
+                        SQL To-do List
+                        </a>
+                    </h3>
+
+                </div>
+                <div>
+                    <h3>
+                        <a target='_blank' href='https://github.com/jeremyacoop/weekend-jquery-server-calculator'>
+                        Server Side Calculator
+                        </a>
+                    </h3>
+                </div>
+            </body>
+
+        </div>
+    );
+
+}
+
+export default App;
